43ec10979a649a2272d678d1b47b094971109eaf
Eugene Eeo <packwolf58@gmail.com>
2015-05-17 17:42:20 +0800

    refactor stuff

--- pygrabbit/parser.py
+++ pygrabbit/parser.py
@@ -1,84 +1,58 @@
-from ._compat import *
+from ._compat import urljoin
 import requests
 from lxml import html
 
 
-headers = {'User-agent': 'PyGrabbit 0.1'}
-
 class PyGrabbit:
-    @staticmethod
-    def url(url):
-        g = PyGrabbit(url)
-        return g
+    headers = {'User-agent': 'PyGrabbit 0.1'}
 
     def __init__(self, url):
         self.url = url
         self._tree = None
-        try:
-            res = requests.get(url, headers=headers)
-            self._content = res.text
-            self._tree = html.fromstring(self._content)
-        except requests.ConnectionError:
-            pass
+        self._content = requests.get(url, headers=self.headers).text
+        self._tree = html.fromstring(self._content)
 
     def _image_absolute_uri(self, image_path):
         return urljoin(self.url, image_path)
 
+    def select(self, *queries):
+        for query in queries:
+            node = self._tree.xpath(query)
+            if node:
+                return node
+        return []
+
     @property
     def title(self):
-        res = None
+        text = self.select(
+            "//meta[@property='og:title']/@content",
+            "//meta[@name='twitter:title']/@content",
+            )
+        if text:
+            return text[0].strip()
 
-        if self._tree is not None:
-            res = self._tree.xpath("//meta[@property='og:title']/@content")
-            # TODO: DRY
-            if res:
-                res = res[0].strip()
-                return res
-            res = self._tree.xpath("//meta[@name='twitter:title']/@content")
-            if res:
-                res = res[0].strip()
-                return res
-            res = self._tree.xpath("//title")
-            if res:
-                res = res[0].text.strip()
-                return res
-        return res
-
-
+        res = self.select("//title")
+        if res:
+            res = res[0].text.strip()
+            return res
 
     @property
     def description(self):
-        res = None
-        if self._tree is not None:
-            res = self._tree.xpath("//meta[@property='og:description']/@content")
-            if res:
-                res = res[0].strip()
-                return res
-            res = self._tree.xpath("//meta[@name='description']/@content")
-            if res:
-                res = res[0].strip()
-                return res
-        return res
+        text = self.select(
+            "//meta[@property='og:description']/@content",
+            "//meta[@name='description']/@content",
+            )
+        if text:
+            return text[0].strip()
 
     @property
     def images(self):
-        res = []
-
-        if self._tree is not None:
-            og = self._tree.xpath('//meta[@property="og:image"]/@content')
-
-            twitter = self._tree.xpath('//meta[@name="twitter:image:src"]/@content')
-
-            main = self._tree.xpath('//img[@id="main-image" or @id="prodImage"]/@src')
-
-            global1 = self._tree.xpath("//img[not(ancestor::*[contains(@id, 'sidebar') or contains(@id, 'comment') or contains(@id, 'footer') or contains(@id, 'header')]) and ancestor::*[contains(@id, 'content')]]/@src")
-
-            global2 = self._tree.xpath("//img[not(ancestor::*[contains(@id, 'sidebar') or contains(@id, 'comment') or contains(@id, 'footer') or contains(@id, 'header')])]/@src")
-
-            global3 = self._tree.xpath("//img/@src")
-
-            res = og or twitter or main or global1 or global2 or global3
-            res = [self._image_absolute_uri(x) for x in res]
-        return res
-
-
+        nodes = self.select(
+            '//meta[@property="og:image"]/@content',
+            '//meta[@name="twitter:image:src"]/@content',
+            '//img[@id="main-image" or @id="prodImage"]/@src',
+            "//img[not(ancestor::*[contains(@id, 'sidebar') or contains(@id, 'comment') or contains(@id, 'footer') or contains(@id, 'header')]) and ancestor::*[contains(@id, 'content')]]/@src",
+            "//img[not(ancestor::*[contains(@id, 'sidebar') or contains(@id, 'comment') or contains(@id, 'footer') or contains(@id, 'header')])]/@src",
+            "//img/@src",
+            )
+        return [self._image_absolute_uri(k) for k in nodes]
--- tests/test_pygrabbit.py
+++ tests/test_pygrabbit.py
@@ -4,79 +4,55 @@ import pytest
 import vcr
 
 
-class TestPyGrabbitUrl:
-    @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
-    def test_typeerror_on_blank_url(self):
-        with pytest.raises(TypeError):
-            g = PyGrabbit.url()
-
-    @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
-    def test_missingschema_on_bad_url(self):
-        with pytest.raises(MissingSchema):
-            g = PyGrabbit.url('hello')
-
-    @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
-    def test_good_url(self):
-        g = PyGrabbit.url('http://www.google.com')
-        assert g is not None
-
-    @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
-    def test_is_not_none_on_404(self):
-        g = PyGrabbit.url('http://www.thisurldoesnotexists.com')
-        assert g.title is None
-        assert g.description is None
-        assert len(g.images) == 0
-
-
 class TestPyGrabbitTitle:
     @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
     def test_title_from_url(self):
-        g = PyGrabbit.url('http://www.drudgereport.com')
+        g = PyGrabbit('http://www.drudgereport.com')
         assert g.title.startswith('DRUDGE REPORT')
 
     @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
     def test_title_from_og(self):
-        g = PyGrabbit.url('http://ogp.me/')
+        g = PyGrabbit('http://ogp.me/')
         assert g.title == 'Open Graph protocol'
 
     @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
     def test_title_twitter_card(self):
-        g = PyGrabbit.url('https://dev.twitter.com/docs/cards/types/summary-card')
+        g = PyGrabbit('https://dev.twitter.com/docs/cards/types/summary-card')
         assert g.title == 'Summary Card'
 
 
 class TestPyGrabbitDescription:
     @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
     def test_description_from_og(self):
-        g = PyGrabbit.url('http://ogp.me/')
+        g = PyGrabbit('http://ogp.me/')
         assert g.description == "The Open Graph protocol enables any web page to become a rich object in a social graph."
 
     @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
     def test_description_from_twitter_card(self):
-        g = PyGrabbit.url("https://dev.twitter.com/docs/cards/types/summary-card")
+        g = PyGrabbit("https://dev.twitter.com/docs/cards/types/summary-card")
         assert g.description == "The Summary Card can be used for many kinds of web content, from blog posts and news articles, to products and restaurants. It is designed to give the reader a preview of the content before clicking through to your website."
 
     @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
     def test_description_from_meta(self):
-        g = PyGrabbit.url("http://moz.com/learn/seo/meta-description")
+        g = PyGrabbit("http://moz.com/learn/seo/meta-description")
         assert g.description == "Get SEO best practices for the meta description tag, including length and content."
 
 
 class TestPyGrabbitImages:
     @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
     def test_return_array(self):
-        g = PyGrabbit.url("http://www.google.com")
+        g = PyGrabbit("http://www.google.com")
         assert type(g.images) == list
 
     @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
     def test_only_images_from_og(self):
-        g = PyGrabbit.url("http://ogp.me/")
+        g = PyGrabbit("http://ogp.me/")
         assert g.images[0] == "http://ogp.me/logo.png"
         assert len(g.images) == 1
 
     @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
     def test_return_images_from_twitter_card(self):
-        g = PyGrabbit.url("http://momwitha.com/2013/08/having-fun-with-pictures-at-google-headquarters/")
+        g = PyGrabbit("http://momwitha.com/2013/08/having-fun-with-pictures-at-google-headquarters/")
         assert g.images[0] == "http://momwitha.com/wp-content/uploads/2013/08/Rita-in-Google-Phonebooth1.jpg"
         assert len(g.images) == 1
 
@@ -90,7 +66,7 @@ class TestPyGrabbitImages:
 
     @vcr.use_cassette('fixtures/vcr_cassettes/pygrabbit.yaml', record_mode='new_episodes')
     def test_return_images_from_global_img(self):
-        g = PyGrabbit.url("http://elixir-lang.org/")
+        g = PyGrabbit("http://elixir-lang.org/")
         assert g.images[0] == "http://elixir-lang.org/images/contents/home-code.png"
         assert len(g.images) == 1
 
