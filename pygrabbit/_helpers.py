e8ee3d6a4b72cdbfe596363f3ee03d59a9e162d7
Esteban (Eka) Feldman <esteban.feldman@gmail.com>
2015-05-18 11:43:31 -0500

    Make it python2 compatible

--- pygrabbit/_helpers.py
+++ pygrabbit/_helpers.py
@@ -1,7 +1,7 @@
 from functools import update_wrapper
 
 
-class cached_attribute:
+class cached_attribute(object):
     def __init__(self, getter):
         self.attr = getter.__name__
         self.getter = getter
